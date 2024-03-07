3913f453ceb93b8314fc13bc821d9fd5734a2595
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2024-03-07 16:28:14 +0100

    Homepage fully developed, can be imporved later

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import ProductCard from '../components/productCard';
+import bannerImage from '../images/banner-image.webp';
+
+const Banner = styled.div`
+  width: 100%;
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-image: url("${bannerImage}");
+    background-size: cover;
+    background-position: center;
+    opacity: 40%;
+  }
+`;
+
+const BannerText = styled.p`
+  font-family: 'Major Mono Display', 'Courier New', Courier, monospace;
+  font-size: 3rem;
+  z-index: 100;
+  user-select: none;
+`
+
+
+
+function HomePage() {
+  return (
+    <main>
+      <Banner title="Banner">
+        <BannerText>Modern Shopping Made Easy</BannerText>
+      </Banner>
+      <ProductCard />
+    </main>
+  );
+}
+
+export default HomePage;
