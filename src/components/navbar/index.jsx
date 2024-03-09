3140fa9160e0724bddd35a3051a17e0ca0230a02
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2024-03-09 22:12:29 +0100

    Navbar is now responsive, could use some fine tuning later

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,20 +1,29 @@
 import { NavLink } from "react-router-dom";
 import styles from "./navbar.module.css";
 import Searchbar from "../searchbar";
+import { useState } from "react";
+
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState("")
+
+  function toggleStyle() {
+    setIsOpen(prevState => !prevState);
+  }
+
   return (
     <nav>
       <NavLink to="/" className={styles.navbar_logo}>
         Checkout
       </NavLink>
       <div className={styles.nav_interactions}>
-        <Searchbar />
-        <ul className={styles.nav_list}>
+        <ul className={`${styles.nav_list} ${isOpen ? styles.open : ''}`}>
+          <Searchbar />
           <NavLink to="/contact">Contact</NavLink>
           <NavLink to="/cart">Price</NavLink>
           <NavLink to="/cart">Cart</NavLink>
         </ul>
+        <button className={styles.nav_toggle} onClick={toggleStyle}></button>
       </div>
     </nav>
   );
