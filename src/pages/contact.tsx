c6c3ff0d4478b460fb6a2fb4b89b8a087ca5bbb1
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2024-03-25 13:45:35 +0100

    Added React Helmet for dynamic meta data

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Banner, BannerText } from './homepage';
 import ContactForm from '../components/contactForm';
+import {Helmet} from "react-helmet";
 
 
 
 function Contact() {
   return (
-    <main>
-      <Banner title="Banner">
-        <BannerText>We're here to help!</BannerText>
-      </Banner>
-      <ContactForm/>
-    </main>
+    <React.Fragment>
+      <Helmet>
+        <meta charSet='UTF-8'></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name='description' content="Come in contact with us!"></meta>
+        <title>Contact Us</title>
+      </Helmet>
+      <main>
+        <Banner title="Banner">
+          <BannerText>We're here to help!</BannerText>
+        </Banner>
+        <ContactForm/>
+      </main>
+    </React.Fragment>
   );
 }
 
